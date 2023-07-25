c24ebb8298336fce3142fd17b20657b42de1763a
梵昀 <nifanyun.nfy@alibaba-inc.com>
2023-07-25 19:30:46 +0800

    feat: add point demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createShaderProgram } from './utils/util';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (!gl) {
+  console.log("抱歉你的浏览器不支持webgl");
+}
+
+const vertexShader = `
+attribute vec4 a_position;
+void main () {
+    gl_Position = a_position;
+    gl_PointSize = 30.;
+}  
+`;
+
+const fragmentShader = `
+    precision mediump float;
+    void main () {
+        gl_FragColor = vec4(1.0, 0.5, 1.0, 1.0);
+    }
+`;
+
+const program = createShaderProgram(gl, vertexShader, fragmentShader);
+gl.useProgram(program!);
+
+const a_position = gl.getAttribLocation(program!, "a_position");
+gl.vertexAttrib3f(a_position, 0.0, 0.0, 0.0);
+
+gl.drawArrays(gl.POINTS, 0, 1);
---
+++ src/utils/util.ts
@@ -0,0 +1,59 @@
+/**
+ * create shader
+ * @param gl 
+ * @param type 
+ * @param source 
+ * @returns 
+ */
+export function createShader(gl: WebGL2RenderingContext, type: number, source: string) {
+  // 创建 shader 对象
+  let shader = gl.createShader(type);
+  // 往 shader 中传入源代码
+  gl.shaderSource(shader!, source);
+  // 编译 shader
+  gl.compileShader(shader!);
+  // 判断 shader 是否编译成功
+  let success = gl.getShaderParameter(shader!, gl.COMPILE_STATUS);
+  if (success) {
+      return shader;
+  }
+  // 如果编译失败，则打印错误信息
+  console.warn(gl.getShaderInfoLog(shader!));
+  gl.deleteShader(shader);
+}
+
+/**
+ * create program
+ * @param gl 
+ * @param vertexShader 
+ * @param fragmentShader 
+ * @returns 
+ */
+export function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
+  // 创建 program 对象
+  let program = gl.createProgram();
+  // 往 program 对象中传入 WebGLShader 对象
+  gl.attachShader(program!, vertexShader);
+  gl.attachShader(program!, fragmentShader);
+  // 链接 program
+  gl.linkProgram(program!);
+  // 判断 program 是否链接成功
+  let success = gl.getProgramParameter(program!, gl.LINK_STATUS);
+  if (success) {
+      return program;
+  }
+  // 如果 program 链接失败，则打印错误信息
+  console.warn(gl.getProgramInfoLog(program!));
+  gl.deleteProgram(program);
+}
+
+export function createShaderProgram(gl: WebGL2RenderingContext, vertexSource: string, fragmentSource: string){
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+  if(vertexShader && fragmentShader){
+    let program = createProgram(gl, vertexShader, fragmentShader) as WebGLProgram;
+    return program;
+  }
+  console.warn('create shader program failed');
+}
+
